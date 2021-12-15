6b7c563542e8738712f4183933e677db627c08ba
magnificGH <guohui36@huawei.com>
2021-12-15 12:12:28 +0800

    delete sharememory codes
    Signed-off-by: magnificGH <guohui36@huawei.com>

--- services/engine/gstreamer/plugins/common/gst_shmem_allocator.h
+++ services/engine/gstreamer/plugins/common/gst_shmem_allocator.h
@@ -17,8 +17,6 @@
 #define GST_SHMEM_ALLOCATOR_H
 
 #include <gst/gst.h>
-#include "gst_shmem_memory.h"
-#include "avsharedmemorypool.h"
 
 G_BEGIN_DECLS
 
@@ -38,7 +36,6 @@ typedef struct _GstShMemAllocatorClass GstShMemAllocatorClass;
 
 struct _GstShMemAllocator {
     GstAllocator parent;
-    std::shared_ptr<OHOS::Media::AVSharedMemoryPool> avShmemPool;
 };
 
 struct _GstShMemAllocatorClass {
@@ -47,10 +44,7 @@ struct _GstShMemAllocatorClass {
 
 GType gst_shmem_allocator_get_type(void);
 
-GstShMemAllocator *gst_shmem_allocator_new();
-
-void gst_shmem_allocator_set_pool(GstShMemAllocator *allocator,
-                                  std::shared_ptr<OHOS::Media::AVSharedMemoryPool> pool);
+GstAllocator *gst_shmem_allocator_new();
 
 G_END_DECLS
 
