b630ab2adf4f9fd22bdf71d17d9a95a5884cb32c
monissa <qiaofenfeng@huawei.com>
2022-07-04 15:50:54 +0800

    add fuzz test case  Signed-off-by: monissaJ<qiaofenfeng@huawei.com>
    Signed-off-by: monissa <qiaofenfeng@huawei.com>

---
+++ test/fuzztest/pixelmap_fuzzer/pixelmap_fuzzer.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "pixelmap_fuzzer.h"
+#include <iostream>
+#include "string_ex.h"
+#include "media_errors.h"
+#include "directory_ex.h"
+#include "ui/rs_surface_node.h"
+#include "window_option.h"
+
+
+using namespace std;
+using namespace OHOS;
+using namespace Media;
+
+PixelMapFuzzer::PixelMapFuzzer()
+{
+}
+PixelMapFuzzer::~PixelMapFuzzer()
+{
+}
+
+bool PixelMapFuzzer::FuzzPixelMap(uint8_t* data, size_t size)
+{
+    metadata_ = AVMetadataHelperFactory::CreateAVMetadataHelper();
+    if (metadata_ == nullptr) {
+        cout << "metadata_ is null" << endl;
+        return false;
+    }
+    cout << "create metadata_ success!" << endl;
+    const string path = "/data/test/resources/H264_AAC.mp4";
+    int32_t ret = MetaDataSetSource(path);
+    if (ret != 0) {
+        cout << "metadata_ SetSource file" << endl;
+        return false;
+    }
+    cout << "metadata_ SetSource success!" << endl;
+    if (size >= sizeof(int64_t)) {
+        struct PixelMapParams pixelMapParams_;
+        std::shared_ptr<PixelMap> ret2 = metadata_ -> FetchFrameAtTime
+        								(*reinterpret_cast<int64_t *>(data), AV_META_QUERY_NEXT_SYNC, pixelMapParams_);
+        if (ret2 != 0) {
+            cout << "metadata_ FetchFrameAtTime fail" << endl;
+            return false;
+        }
+    }
+    cout << "metadata_ FetchFrameAtTime success!" << endl;
+    metadata_ -> Release();
+    cout << "success!" << endl;
+    return true;
+}
+
+bool OHOS::Media::FuzzTestPixelMap(uint8_t* data, size_t size)
+{
+    auto player = std::make_unique<PixelMapFuzzer>();
+    if (player == nullptr) {
+        cout << "player is null" << endl;
+        return 0;
+    }
+    return player -> FuzzPixelMap(data, size);
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Media::FuzzTestPixelMap(data, size);
+    return 0;
+}
+
