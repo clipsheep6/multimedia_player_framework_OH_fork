d2c1ae066a6e8e7ffb98288b04fd124270bf7598
sharpshooter_t <taojun13@huawei.com>
2022-07-20 22:26:56 +0800

    add patch from master
    Signed-off-by: sharpshooter_t <taojun13@huawei.com>

--- services/engine/gstreamer/common/playbin_adapter/playbin_ctrler_base.cpp
+++ services/engine/gstreamer/common/playbin_adapter/playbin_ctrler_base.cpp
@@ -33,6 +33,7 @@ namespace {
     constexpr uint64_t BUFFER_DURATION = 15000000000; // 15s
     constexpr int32_t BUFFER_LOW_PERCENT_DEFAULT = 1;
     constexpr int32_t BUFFER_HIGH_PERCENT_DEFAULT = 4;
+    constexpr int32_t BUFFER_PERCENT_THRESHOLD = 100;
     constexpr uint32_t HTTP_TIME_OUT_DEFAULT = 15; // 15s
     constexpr int32_t NANO_SEC_PER_USEC = 1000;
     constexpr double DEFAULT_RATE = 1.0;
@@ -316,9 +317,14 @@ int32_t PlayBinCtrlerBase::SetRateInternal(double rate)
 {
     MEDIA_LOGD("execute set rate, rate: %{public}lf", rate);
 
-    gint64 position = 0;
-    gboolean ret = gst_element_query_position(GST_ELEMENT_CAST(playbin_), GST_FORMAT_TIME, &position);
-    CHECK_AND_RETURN_RET_LOG(ret, MSERR_NO_MEMORY, "query position failed");
+    gint64 position;
+    gboolean ret;
+    if (isDuration_) {
+        position = duration_ * NANO_SEC_PER_USEC;
+    } else {
+        ret = gst_element_query_position(GST_ELEMENT_CAST(playbin_), GST_FORMAT_TIME, &position);
+        CHECK_AND_RETURN_RET_LOG(ret, MSERR_NO_MEMORY, "query position failed");
+    }
 
     GstSeekFlags flags = ChooseSetRateFlags(rate);
     int64_t start = rate > 0 ? position : 0;
@@ -450,6 +456,7 @@ void PlayBinCtrlerBase::Reset() noexcept
     isSeeking_ = false;
     isRating_ = false;
     isBuffering_ = false;
+    isDuration_ = false;
 
     MEDIA_LOGD("exit");
 }
@@ -776,6 +783,7 @@ void PlayBinCtrlerBase::ProcessEndOfStream()
 {
     MEDIA_LOGD("End of stream");
     std::unique_lock<std::mutex> lock(mutex_);
+    isDuration_ = true;
     if (IsLiveSource()) {
         MEDIA_LOGD("appsrc livemode, can not loop");
         return;
@@ -822,7 +830,8 @@ void PlayBinCtrlerBase::HandleCacheCtrl(const InnerMessage &msg)
 
 void PlayBinCtrlerBase::HandleCacheCtrlWhenNoBuffering(int32_t percent)
 {
-    if (percent < BUFFER_LOW_PERCENT_DEFAULT) {
+    if (percent < static_cast<float>(BUFFER_LOW_PERCENT_DEFAULT) / BUFFER_HIGH_PERCENT_DEFAULT *
+        BUFFER_PERCENT_THRESHOLD) {
         isBuffering_ = true;
         {
             std::unique_lock<std::mutex> lock(mutex_);
@@ -843,7 +852,7 @@ void PlayBinCtrlerBase::HandleCacheCtrlWhenNoBuffering(int32_t percent)
 
 void PlayBinCtrlerBase::HandleCacheCtrlWhenBuffering(int32_t percent)
 {
-    if (percent > BUFFER_HIGH_PERCENT_DEFAULT) {
+    if (percent >= BUFFER_PERCENT_THRESHOLD) {
         isBuffering_ = false;
         if (GetCurrState() == playingState_) {
             {
