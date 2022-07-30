16274d35e2bce1b70595cd3c3ee6bbc926147e61
majingtao1 <majingtao1@huawei.com>
2022-07-30 18:31:43 +0800

    2.4.7音视频封装功能
    Signed-off-by: majingtao1 <majingtao1@huawei.com>

--- test/nativedemo/avmuxer/avmuxer_demo.h
+++ test/nativedemo/avmuxer/avmuxer_demo.h
@@ -29,26 +29,33 @@ private:
     bool PushBuffer(std::shared_ptr<std::ifstream> File, const int32_t frameSize,
         int32_t i, int32_t trackId, int64_t stamp);
     void WriteTrackSample();
-    void SetParameter(const std::string &type);
+    void SetParameter(const std::string &type, int32_t trackNum = 0);
     bool AddTrackVideo(std::string &videoType);
-    bool AddTrackAudio(std::string &audioType);
+    bool AddTrackAudio(std::string &audioType, int32_t trackNum = 0);
     void DoNext();
     void SetMode(int32_t mode);
     std::shared_ptr<AVMuxer> avmuxer_;
     int32_t videoTrackId_ = 0;
     int32_t audioTrackId_ = 0;
+    int32_t audioTrackId_1_ = 0;
     uint32_t videoTimeDuration_ = 0;
     uint32_t audioTimeDuration_ = 0;
+    uint32_t audioTimeDuration_1_ = 0;
     uint32_t videoFrameNum_ = 0;
     uint32_t audioFrameNum_ = 0;
+    uint32_t audioFrameNum_1_ = 0;
     const int32_t *videoFrameArray_ = nullptr;
     const int32_t *audioFrameArray_ = nullptr;
+    const int32_t *audioFrameArray_1_ = nullptr;
     std::shared_ptr<std::ifstream> videoFile_ = nullptr;
     std::shared_ptr<std::ifstream> audioFile_ = nullptr;
+    std::shared_ptr<std::ifstream> audioFile_1_ = nullptr;
     std::string videoType_ = std::string("");
     std::string audioType_ = std::string("");
+    std::string audioType_1_ = std::string("");
     std::string path_ = std::string("");
     std::string format_ = std::string("");
+    bool isMultAudioTrack = false;
 };
 }  // namespace Media
 }  // namespace OHOS
