f0ac93ceba70fa97575ae4e866bee466ab259d9a
monissa <qiaofenfenghuawei.com>
2022-09-09 18:09:05 +0800

    fuzz：RecorderSetAudioSourceFuzzTest/RecorderSetAudioChannelsFuzzTes Signed-off-by: monissa<qiaofenfeng@huawei.com>
    Signed-off-by: monissa <qiaofenfenghuawei.com>

--- test/fuzztest/test_recorder.cpp
+++ test/fuzztest/test_recorder.cpp
@@ -524,7 +524,7 @@ void TestRecorder::HDICreateESBuffer()
         (void)buffer->GetExtraData()->ExtraSet("dataSize", static_cast<int32_t>(*frameLenArray));
         (void)buffer->GetExtraData()->ExtraSet("timeStamp", pts);
         (void)buffer->GetExtraData()->ExtraSet("isKeyFrame", isKeyFrame);
-        ((++nowFrame % 30) == 0) ? (isKeyFrame = 1) : (isKeyFrame = 0);
+        ((++nowFrame % 30) == 0) ? (isKeyFrame = 1) : (isKeyFrame = 0);   // keyframe every 30fps
         pts += FRAME_DURATION;
         (void)producerSurface->FlushBuffer(buffer, -1, g_esFlushConfig);
         frameLenArray++;
