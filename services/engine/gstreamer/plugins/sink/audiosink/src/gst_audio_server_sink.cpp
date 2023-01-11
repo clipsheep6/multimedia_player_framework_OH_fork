9ac016fdeaa72ce06c702f972f534dfffe8ed794
sqwlly <shiqiwei4@huawei.com>
2023-01-11 02:12:16 +0000

    update services/engine/gstreamer/plugins/sink/audiosink/src/gst_audio_server_sink.cpp.
    Signed-off-by: sqwlly <shiqiwei4@huawei.com>

--- services/engine/gstreamer/plugins/sink/audiosink/src/gst_audio_server_sink.cpp
+++ services/engine/gstreamer/plugins/sink/audiosink/src/gst_audio_server_sink.cpp
@@ -442,12 +442,9 @@ static gboolean gst_audio_server_sink_event(GstBaseSink *basesink, GstEvent *eve
             break;
         case GST_EVENT_STREAM_GROUP_DONE:
             basesink->stream_group_done = TRUE;
-            if (basesink->need_preroll) {
-                GST_DEBUG_OBJECT (basesink, "async start to change state, preroll"
-                "STREAM_GROUP_DONE to async done");
-                gst_base_sink_do_preroll (basesink, GST_MINI_OBJECT_CAST(event));
-            }
             GST_DEBUG_OBJECT(basesink, "received STREAM_GROUP_DONE, set stream_group_done TRUE");
+            /* may async start to change state, preroll STREAM_GROUP_DONE to async done */
+            gst_base_sink_do_preroll (basesink, GST_MINI_OBJECT_CAST(event));
             break;
         case GST_EVENT_STREAM_START:
             basesink->stream_group_done = FALSE;
