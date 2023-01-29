ae464f1d00e522ba2c737ab133ad753b001e9ebe
QWER321 <zhangming184@huawei.com>
2023-01-29 15:05:41 +0800

    Add Purgeable Memory
    Signed-off-by: QWER321 <zhangming184@huawei.com>

--- services/services/player/server/player_server.cpp
+++ services/services/player/server/player_server.cpp
@@ -465,7 +465,7 @@ int32_t PlayerServer::Release()
 {
     std::lock_guard<std::mutex> lock(mutex_);
     {
-        std::lock_guard<std::mutex> lockCb(mutexCb_);
+        std::lock_guard<std::recursive_mutex> lockCb(recMutexCb_);
         playerCb_ = nullptr;
     }
     MEDIA_LOGD("PlayerServer Release in");
@@ -906,9 +906,22 @@ int32_t PlayerServer::SetPlayerCallback(const std::shared_ptr<PlayerCallback> &c
     }
 
     {
-        std::lock_guard<std::mutex> lockCb(mutexCb_);
+        std::lock_guard<std::recursive_mutex> lockCb(recMutexCb_);
         playerCb_ = callback;
     }
+    return MSERR_OK;
+}
+
+void PlayerServer::SetPlayerCallbackInner(const std::shared_ptr<PlayerCallback> &callback)
+{
+    std::lock_guard<std::recursive_mutex> lockCb(recMutexCb_);
+    playerCb_ = callback;
+}
+
+int32_t PlayerServer::SetObs(const std::weak_ptr<IPlayerEngineObs> &obs)
+{
+    std::unique_lock<std::mutex> lock(mutex_);
+    obs_ = obs;
     return MSERR_OK;
 }
 
@@ -927,7 +940,6 @@ int32_t PlayerServer::DumpInfo(int32_t fd)
         dumpString +=
             "The engine is not created, note: engine can't be created until set source.\n";
             write(fd, dumpString.c_str(), dumpString.size());
-        return MSERR_OK;
     }
     dumpString += "PlayerServer current state is: " + GetStatusDescription(lastOpStatus_) + "\n";
     if (lastErrMsg_.size() != 0) {
@@ -967,7 +979,7 @@ void PlayerServer::OnError(PlayerErrorType errorType, int32_t errorCode)
 
 void PlayerServer::OnErrorMessage(int32_t errorCode, const std::string &errorMsg)
 {
-    std::lock_guard<std::mutex> lockCb(mutexCb_);
+    std::lock_guard<std::recursive_mutex> lockCb(recMutexCb_);
     lastErrMsg_ = errorMsg;
     FaultEventWrite(lastErrMsg_, "Player");
     if (playerCb_ != nullptr && !errorCbOnce_) {
@@ -978,7 +990,7 @@ void PlayerServer::OnErrorMessage(int32_t errorCode, const std::string &errorMsg
 
 void PlayerServer::OnInfo(PlayerOnInfoType type, int32_t extra, const Format &infoBody)
 {
-    std::lock_guard<std::mutex> lockCb(mutexCb_);
+    std::lock_guard<std::recursive_mutex> lockCb(recMutexCb_);
 
     if (type == INFO_TYPE_STATE_CHANGE_BY_AUDIO && extra == PLAYER_PAUSED && lastOpStatus_ == PLAYER_STARTED) {
         isStateChangedBySystem_ = true;
@@ -986,14 +998,19 @@ void PlayerServer::OnInfo(PlayerOnInfoType type, int32_t extra, const Format &in
     } else {
         int32_t ret = HandleMessage(type, extra, infoBody);
         if (playerCb_ != nullptr && ret == MSERR_OK) {
+            MEDIA_LOGD("OnInfo user type:%{public}d, extra:%{public}d", type, extra);
             playerCb_->OnInfo(type, extra, infoBody);
+        }
+        std::shared_ptr<IPlayerEngineObs> notifyObs = obs_.lock();
+        if (notifyObs != nullptr) {
+            notifyObs->OnInfo(type, extra, infoBody);
         }
     }
 }
 
 void PlayerServer::OnInfoNoChangeStatus(PlayerOnInfoType type, int32_t extra, const Format &infoBody)
 {
-    std::lock_guard<std::mutex> lockCb(mutexCb_);
+    std::lock_guard<std::recursive_mutex> lockCb(recMutexCb_);
 
     if (playerCb_ != nullptr) {
         playerCb_->OnInfo(type, extra, infoBody);
