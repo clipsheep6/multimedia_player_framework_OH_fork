766a057d6ce70a6c5a877cb6542b7c708ae3cfc0
x30034819 <xieyijun3@huawei.com>
2023-01-29 09:51:22 +0800

    录制输入超时报错 Signed-off-by: x30034819 <xieyijun3@huawei.com>
    Signed-off-by: x30034819 <xieyijun3@huawei.com>
    Change-Id: Ie20cf9d04272eb096914d60705019093e130ede3

--- services/utils/watchdog.cpp
+++ services/utils/watchdog.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 
+#include <thread>
 #include "watchdog.h"
-#include "media_log.h"
 
 namespace {
     constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN, "watchdog"};
