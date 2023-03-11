ee7514168de8467399b0abaf81f1cc2caaaf0718
sqwlly <shiqiwei4@huawei.com>
2023-03-11 10:21:50 +0800

    变codec时，先stop当前decoder Signed-off-by: s30029175 <shiqiwei4@huawei.com>
    Signed-off-by: sqwlly <shiqiwei4@huawei.com>
    Change-Id: I106479ab526a6c152b1c73a696e0b4344815b9c5

--- services/engine/gstreamer/plugins/codec/common/vdec/gst_vdec_base.cpp
+++ services/engine/gstreamer/plugins/codec/common/vdec/gst_vdec_base.cpp
@@ -1582,7 +1582,7 @@ static void gst_vdec_base_need_stop(GstVideoDecoder *decoder)
     GstVdecBase *self = GST_VDEC_BASE(decoder);
     g_return_if_fail(self != nullptr);
     GstPad *srcpad = GST_VIDEO_DECODER_SRC_PAD(self);
-    if (gst_pad_get_task_state(pad) != GST_TASK_STARTED) {
+    if (gst_pad_get_task_state(srcpad) != GST_TASK_STARTED) {
         GST_DEBUG_OBJECT(self, "vdec not start yet");
         return;
     }
