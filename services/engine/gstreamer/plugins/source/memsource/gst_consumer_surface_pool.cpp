5d188cda157bfa780b2754420001ffedc86c3870
x30034819 <xieyijun3@huawei.com>
2023-03-25 16:06:58 +0800

    ci dump test Signed-off-by: x30034819 <xieyijun3@huawei.com>
    Signed-off-by: x30034819 <xieyijun3@huawei.com>
    Change-Id: I8d1359ced41c40bd780468044d38babdbf7f7c95

--- services/engine/gstreamer/plugins/source/memsource/gst_consumer_surface_pool.cpp
+++ services/engine/gstreamer/plugins/source/memsource/gst_consumer_surface_pool.cpp
@@ -702,7 +702,7 @@ static void gst_consumer_surface_pool_get_dump_file(GstConsumerSurfacePool *pool
     }
 }
 
-static void gst_consumer_surface_pool_dump_data(FILE *dump_file, const void *addr,
+static void gst_consumer_surface_pool_dump_data(GstConsumerSurfacePool *pool, FILE *dump_file, const void *addr,
     gint32 size, gint32 width, gint32 height)
 {
     g_return_if_fail(dump_file != nullptr && addr != nullptr);
@@ -741,7 +741,7 @@ static void gst_consumer_surface_pool_dump_surfacebuffer(GstConsumerSurfacePool 
         (void)extraData->ExtraGet("dataSize", data_size);
     }
 
-    gst_consumer_surface_pool_dump_data(pool->priv->dump_file, buffer->GetVirAddr(),
+    gst_consumer_surface_pool_dump_data(pool, pool->priv->dump_file, buffer->GetVirAddr(),
         data_size, buffer->GetWidth(), buffer->GetHeight());
     return;
 }
@@ -758,7 +758,7 @@ static void gst_consumer_surface_pool_dump_gstbuffer(GstConsumerSurfacePool *poo
     g_return_if_fail(meta != nullptr);
     GstMapInfo info = GST_MAP_INFO_INIT;
     gst_buffer_map(buf, &info, GST_MAP_READ);
-    gst_consumer_surface_pool_dump_data(pool->priv->dump_file, info.data,
+    gst_consumer_surface_pool_dump_data(pool, pool->priv->dump_file, info.data,
         static_cast<gint32>(meta->length), static_cast<gint32>(meta->width), static_cast<gint32>(meta->height));
     gst_buffer_unmap(buf, &info);
     return;
