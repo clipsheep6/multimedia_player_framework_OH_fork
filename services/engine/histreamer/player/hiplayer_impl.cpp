2bf42304f4c5af1a828b6289b8e9ed2c4a26a1d8
chongzhi <caihongzhi@huawei.com>
2024-04-13 17:29:13 +0800

    Prepare处理首帧
    Signed-off-by: chongzhi <caihongzhi@huawei.com>

--- services/engine/histreamer/player/hiplayer_impl.cpp
+++ services/engine/histreamer/player/hiplayer_impl.cpp
@@ -277,6 +277,8 @@ int32_t HiPlayerImpl::PrepareAsync()
         MEDIA_LOG_E("PrepareAsync failed with error " PUBLIC_LOG_D32, ret);
         return TransStatus(ret);
     }
+    ret = pipeline_->PrepareFrame(false);
+    FALSE_RETURN_V_MSG_E(ret == Status::OK, TransStatus(ret), "PrepareFrame failed.");
     NotifyBufferingUpdate(PlayerKeys::PLAYER_BUFFERING_END, 0);
     InitDuration();
     NotifyDurationUpdate(PlayerKeys::PLAYER_CACHED_DURATION, durationMs_.load());
