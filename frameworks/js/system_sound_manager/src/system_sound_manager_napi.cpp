7bee4e9c9d3e01b83821c39e13d6682d10799374
zhaijunwei <zhaijunwei123@huawei.com>
2024-06-03 19:01:47 +0800

    modify ringtone attrs
    Signed-off-by: zhaijunwei <zhaijunwei123@huawei.com>

--- frameworks/js/system_sound_manager/src/system_sound_manager_napi.cpp
+++ frameworks/js/system_sound_manager/src/system_sound_manager_napi.cpp
@@ -1675,7 +1675,7 @@ void SystemSoundManagerNapi::AsyncAddCustomizedTone(napi_env env, void *data)
             context->abilityContext_, context->toneAttrsNapi->GetToneAttrs(), context->externalUri);
         context->status = SUCCESS;
     } else if (context->fd > 0) {
-        if (context->offset > 0 && context->length > 0) {
+        if (context->offset >= 0 && context->length >= 0) {
             context->uri = context->objectInfo->sysSoundMgrClient_->AddCustomizedToneByFdAndOffset(
                 context->abilityContext_, context->toneAttrsNapi->GetToneAttrs(),
                 context->fd, context->offset, context->length);
